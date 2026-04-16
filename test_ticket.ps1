2aa2ae91f1be51754632182844bbe110f6c16608
Gee Suajay <surajmohammedbwoy@gmail.com>
2026-04-16 19:37:24 +0000

    Apr 16, 2026, 7:37 PM

--- test_ticket.ps1
+++ test_ticket.ps1
@@ -4,23 +4,15 @@ $lr = Invoke-RestMethod -Uri "http://localhost:4000/api/v1/players/login" -Metho
 $t = $lr.access_token
 $uid = $lr.profile.id
 
-# Try issuing a ticket with a fake schedule ID
-$ticketBody = @{
-    game_code = "PS5DRAW"
-    game_schedule_id = "direct-ef6b4f8a-9023-442c-aaa6-40b5a7e2f791"
-    draw_number = 1
-    selected_numbers = @()
-    bet_lines = @(@{
-        line_number = 1
-        bet_type = "RAFFLE"
-        selected_numbers = @()
-        total_amount = 1000
-    })
-    customer_phone = "+233256826832"
-    customer_name = "Suraj Mohammed"
-    payment_method = "mobile_money"
-    payment_ref = "manual-test-123"
-} | ConvertTo-Json -Depth 5
+# Use PS5 schedule ID
+$scheduleId = "cb09018b-e52b-46f4-9ae1-5a7fbd197aa2"
 
-$res = Invoke-RestMethod -Uri "http://localhost:4000/api/v1/players/$uid/tickets" -Method POST -ContentType "application/json" -Headers @{Authorization="Bearer $t"} -Body $ticketBody
-$res | ConvertTo-Json -Depth 5
+$ticketBody = '{"game_code":"PS5DRAW","game_schedule_id":"' + $scheduleId + '","draw_number":1,"selected_numbers":[],"bet_lines":[{"line_number":1,"bet_type":"RAFFLE","selected_numbers":[],"total_amount":1000}],"customer_phone":"+233256826832","customer_name":"Suraj Mohammed","payment_method":"mobile_money","payment_ref":"manual-test-123"}'
+
+try {
+    $res = Invoke-RestMethod -Uri "http://localhost:4000/api/v1/players/$uid/tickets" -Method POST -ContentType "application/json" -Headers @{Authorization="Bearer $t"} -Body $ticketBody
+    Write-Host "SUCCESS"
+    $res | ConvertTo-Json -Depth 5
+} catch {
+    Write-Host "FAILED: $($_.ErrorDetails.Message)"
+}
