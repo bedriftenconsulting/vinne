1ee8579b51512e3ac3db6d05b21eb2d3c3d29386
Gee Suajay <surajmohammedbwoy@gmail.com>
2026-04-16 19:33:59 +0000

    Apr 16, 2026, 7:33 PM

---
+++ test_ticket.ps1
@@ -0,0 +1,26 @@
+$pwd = 'P5r2n4x3100$$'
+$body = "{`"phone_number`":`"+233256826832`",`"password`":`"$pwd`",`"device_id`":`"web_test`",`"channel`":`"WEBSITE`",`"device_info`":{`"device_type`":`"web`",`"os`":`"web`",`"os_version`":`"web`",`"app_version`":`"1.0.0`",`"user_agent`":`"test`"}}"
+$lr = Invoke-RestMethod -Uri "http://localhost:4000/api/v1/players/login" -Method POST -ContentType "application/json" -Body $body
+$t = $lr.access_token
+$uid = $lr.profile.id
+
+# Try issuing a ticket with a fake schedule ID
+$ticketBody = @{
+    game_code = "PS5DRAW"
+    game_schedule_id = "direct-ef6b4f8a-9023-442c-aaa6-40b5a7e2f791"
+    draw_number = 1
+    selected_numbers = @()
+    bet_lines = @(@{
+        line_number = 1
+        bet_type = "RAFFLE"
+        selected_numbers = @()
+        total_amount = 1000
+    })
+    customer_phone = "+233256826832"
+    customer_name = "Suraj Mohammed"
+    payment_method = "mobile_money"
+    payment_ref = "manual-test-123"
+} | ConvertTo-Json -Depth 5
+
+$res = Invoke-RestMethod -Uri "http://localhost:4000/api/v1/players/$uid/tickets" -Method POST -ContentType "application/json" -Headers @{Authorization="Bearer $t"} -Body $ticketBody
+$res | ConvertTo-Json -Depth 5
